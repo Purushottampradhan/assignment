ef41d3b586b81942197b9272f62df89741deef06
purushottam <purushottampradhan97@gmail.com>
2021-01-12 16:15:13 +0530

    code changed

--- js2.js
+++ js2.js
@@ -4,7 +4,7 @@ label1.innerText="Name:"
 
 let label2=document.createElement('label')
 label2.setAttribute('for','inputclass')
-label2.innerHTML="</br></br>Class:"
+label2.innerHTML="<br><br>Class:"
 
 let inputname=document.createElement('input')
 inputname.id="inputname"
@@ -17,6 +17,7 @@ label3.innerHTML="</br></br>"
 
 let submitbtn=document.createElement('input')
 submitbtn.type='submit'
+submitbtn.id="subbtn"
 
 document.body.appendChild(label1)
 document.body.appendChild(inputname)
@@ -24,6 +25,26 @@ document.body.appendChild(label2)
 document.body.appendChild(inputclass)
 document.body.appendChild(label3)
 document.body.appendChild(submitbtn)
+
+submitbtn.addEventListener('click',myFunction);
+
+function myFunction(){
+    let stdname=inputname.value;
+    let stdclass=inputclass.value;
+
+    console.log(stdclass);
+    console.log();
+    if(stdname=='' || stdclass==''){
+        alert("Please enter Name and Class")
+    }
+    else{
+        
+        let arr=[stdname,stdclass];
+        localStorage.setItem("studentdetails",arr);
+        // alert("Submited");
+        
+    }
+}
 
 
 
